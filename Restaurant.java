e0f0aee23cec3eed9b2090754c47c2bbf02bd375
Matthew Hague <matthew.hague@rhul.ac.uk>
2020-09-18 10:49:57 +0100

    Add address to Restaurant class

--- Restaurant.java
+++ Restaurant.java
@@ -2,12 +2,17 @@
 public class Restaurant {
     private String name;
     private String cuisine;
+    private String address;
 
-    public class Restaurant(String name, String cuisine) {
+    public class Restaurant(String name,
+                            String cuisine,
+                            String address) {
         this.name = name;
         this.cuisine = cuisine;
+        this.address = address;
     }
 
     public String getName() { return name; }
     public String getCuisine() { return cuisine; }
+    public String getAddress() { return address; }
 }
